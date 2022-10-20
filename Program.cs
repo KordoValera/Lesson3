34ed93cf670bb504b1291c3748948878430a9d57
Valeriia Kordo <lisa_lera@inbox.ru>
2022-10-21 01:15:39 +0300

    Решила задачу на семинаре

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу квадратов чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int number = 1;
+
+while(count <= N)
+{
+    number = count*count;
+    count ++;
+    Console.Write($"{number}, ");
+}
